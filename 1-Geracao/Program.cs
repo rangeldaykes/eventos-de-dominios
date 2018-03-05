0ab3d62ff319a84a3c4222bc0c9126602f70dfb8
rangel daykes <daykes@gmail.com>
2018-03-04 21:39:03 -0300

    alterar nomes das pastas com numeros

---
+++ 1-Geracao/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Geracao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            System.Console.ReadLine();
+        }
+    }
+}
---
+++ 2-Disparo_e_Consumo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Geracao.Services;
+
+namespace Geracao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var cc = new ContaCorrente("rangel");
+            cc.Lancar(new ContaCorrente.Lancamento("Conta Gás", -1000M));
+            
+
+            System.Console.ReadLine();
+        }
+    }
+}
